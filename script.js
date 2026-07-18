5cf1a2a6ad3e28067f307df9e56428f373621ec9
ajis77697 <ajis77697@gmail.com>
2026-07-18 14:29:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function bukaSurat(){
+
+
+document.getElementById("opening").style.display="none";
+
+
+document.getElementById("content").style.display="block";
+
+
+let musik=document.getElementById("musik");
+
+
+musik.play();
+
+
+}
+
+
+
+function playMusic(){
+
+
+let musik=document.getElementById("musik");
+
+
+musik.play();
+
+
+}
+
+
+
+
+function kejutan(){
+
+
+document.getElementById("pesan").innerHTML=
+
+
+"🤍 TETEP JADI REGA YANG GAREN KENAL YA JANGAN BERUBAH, LOVE YOU MAMAS 💗";
+
+
+}
